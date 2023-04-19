0c7c359c46639b456813c82c7eb27cc5a56b4ebc
심승용 <seungyongshim@nexon.co.kr>
2023-04-20 08:49:12 +0900

    .

---
+++ src/BlazorApp1/Pages/Index.razor.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.JSInterop;
+using BlazorApp1;
+using BlazorApp1.Shared;
+using BootstrapBlazor.Components;
+using PhoneNumbers;
+using System.Text.Json;
+
+namespace BlazorApp1.Pages
+{
+    public partial class Index
+    {
+        public string? Text { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Text = """
+            {
+                "Number": "0911-518-067",
+                "Country": "TW"
+            }
+            """;
+
+            OnChange(new ChangeEventArgs { Value = Text });
+        }
+
+        static PhoneNumberUtil PhoneNumberUtil = PhoneNumberUtil.GetInstance();
+        string Number1 { get; set; }
+
+        void OnChange(ChangeEventArgs e)
+        {
+            if (e.Value is string m)
+            {
+                try
+                {
+                    var x = JsonSerializer.Deserialize<Phone>(m);
+
+                    var result = PhoneNumberUtil.Parse(x.Number, x.Country);
+
+                    Number1 = $"{result.CountryCode}{result.NationalNumber}";
+                }
+                catch
+                {
+                    Number1 = "Invalid";
+                }
+            }
+        }
+
+        public record Phone
+        {
+            public string Number { get; init; }
+
+            public string Country { get; init; }
+        }
+    }
+}
